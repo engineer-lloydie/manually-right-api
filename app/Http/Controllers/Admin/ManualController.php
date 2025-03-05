64f67d9b7e21702646aa43c0f7cffcdec63d2f60
lloydie <lloydie.glophics@gmail.com>
2025-03-05 20:30:25 +0800

    Updates

--- app/Http/Controllers/Admin/ManualController.php
+++ app/Http/Controllers/Admin/ManualController.php
@@ -146,9 +146,7 @@ class ManualController extends Controller
 
     public function getLatestProducts() {
         try {
-            $manuals = Manual::with(['thumbnails' => function($query) {
-                    $query->first();
-                }])
+            $manuals = Manual::with('thumbnails')
                 ->leftJoin('sub_categories', 'manuals.id', '=', 'sub_categories.id')
                 ->leftJoin('main_categories', 'sub_categories.main_category_id', '=', 'main_categories.id')
                 ->whereHas('thumbnails')
