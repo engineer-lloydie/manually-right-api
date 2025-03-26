52c39b71076cc2e74bece4d6ff1dce9bdb08f1a3
engineer-lloydie <adolfojohnlloyd5@gmail.com>
2025-03-26 20:36:16 +0800

    Updated the Sitemap Content API

--- app/Http/Controllers/SitemapUrlController.php
+++ app/Http/Controllers/SitemapUrlController.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\DB;
 class SitemapUrlController extends Controller
 {
     public function getURLs() {
-        $url = collect();
-
-        $baseUrls = DB::table('manuals')
+        return DB::table('manuals')
             ->leftJoin('sub_categories', 'manuals.sub_category_id', '=', 'sub_categories.id')
             ->leftJoin('main_categories', 'sub_categories.main_category_id', '=', 'main_categories.id')
             ->select(
@@ -17,9 +15,8 @@ class SitemapUrlController extends Controller
                 'sub_categories.url_slug as sub_category_url',
                 'main_categories.url_slug as main_category_url'
             )
-            ->get();
-
-        $urls = $baseUrls->map(function ($item) {
+            ->get()
+            ->map(function ($item) {
                 return [
                     'url' => "/manuals/categories/{$item->main_category_url}/{$item->sub_category_url}/{$item->manual_url}",
                     'lastmod' => now()->toDateString(),
@@ -27,27 +24,5 @@ class SitemapUrlController extends Controller
                     'priority' => '0.9'
                 ];
             });
-
-        $subCategoryUrls = $baseUrls->unique('sub_category_url')
-            ->map(function ($item) {
-                return [
-                    'url' => "/manuals/categories/{$item->main_category_url}/{$item->sub_category_url}",
-                    'lastmod' => now()->toDateString(),
-                    'changefreq' => 'daily',
-                    'priority' => '0.8'
-                ];
-            });
-
-        $mainCategoryUrls = $baseUrls->unique('main_category_url')
-            ->map(function ($item) {
-                return [
-                    'url' => "/manuals/categories/{$item->main_category_url}",
-                    'lastmod' => now()->toDateString(),
-                    'changefreq' => 'daily',
-                    'priority' => '0.8'
-                ];
-            });
-
-        return $urls->merge($subCategoryUrls)->merge($mainCategoryUrls);
     }
 }
