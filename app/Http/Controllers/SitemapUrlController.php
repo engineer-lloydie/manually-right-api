3f345c4be2c9179caa2fbd5be177f09892c5a21d
engineer-lloydie <adolfojohnlloyd5@gmail.com>
2025-03-26 21:16:46 +0800

    Updated the Sitemap URL API

--- app/Http/Controllers/SitemapUrlController.php
+++ app/Http/Controllers/SitemapUrlController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 class SitemapUrlController extends Controller
 {
     public function getURLs() {
-        return DB::table('manuals')
+        $baseUrls = DB::table('manuals')
             ->leftJoin('sub_categories', 'manuals.sub_category_id', '=', 'sub_categories.id')
             ->leftJoin('main_categories', 'sub_categories.main_category_id', '=', 'main_categories.id')
             ->select(
@@ -15,8 +15,9 @@ class SitemapUrlController extends Controller
                 'sub_categories.url_slug as sub_category_url',
                 'main_categories.url_slug as main_category_url'
             )
-            ->get()
-            ->map(function ($item) {
+            ->get();
+
+        $productUrls = $baseUrls->map(function ($item) {
                 return [
                     'url' => "/manuals/categories/{$item->main_category_url}/{$item->sub_category_url}/{$item->manual_url}",
                     'lastmod' => now()->toDateString(),
@@ -24,5 +25,17 @@ class SitemapUrlController extends Controller
                     'priority' => '0.9'
                 ];
             });
+
+        $subCategoryUrls = $baseUrls->unique('sub_category_url')
+            ->map(function ($item) {
+                return [
+                    'url' => "/manuals/categories/{$item->main_category_url}/{$item->sub_category_url}",
+                    'lastmod' => now()->toDateString(),
+                    'changefreq' => 'daily',
+                    'priority' => '0.8'
+                ];
+            });
+
+        return $productUrls->merge($subCategoryUrls);
     }
 }
